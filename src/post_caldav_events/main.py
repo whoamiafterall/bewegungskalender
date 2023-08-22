ea80cd296361c98afa794ce455adebb8e652af7a
Whoami <aberallesigut@riseup.net>
2023-08-22 20:04:24 +0200

    Moved Newsletter Recipients to Config and CLI

--- src/post_caldav_events/main.py
+++ src/post_caldav_events/main.py
@@ -21,6 +21,7 @@ def get_args (override_args = None):
     argparser.add_argument("-u", "--update-events", dest='update_events', help='check Mailbox for new events and add them to calendar', action='store_true')
     argparser.add_argument("-m", "--map", dest='update_map', help='create MapData in geojson from loaction entries of events', action='store_true')
     argparser.add_argument("-n", "--newsletter", dest='send_newsletter', help='send email-newsletter', action='store_true')
+    argparser.add_argument("-r", "--recipients", dest='recipient', help='override newsletter recipients from config - useful for testing')
     argparser.add_argument("-t", "--telegram", dest='send_telegram', help='send message to telegram', action='store_true')
     argparser.add_argument("-tid", "--telegram-id", dest='telegram_id', help='override telegram_id from config - useful if you have a channel for testing and one for production')
     argparser.set_defaults(config_file="config.yml", query_start=1, query_end=1, telegram_id=None)
@@ -58,7 +59,8 @@ def main(events = {}):
     if args.update_map:
         createMapData(events)
     if args.send_newsletter:
-        send_newsletter(config, querystart, queryend, events)
+        recipient = args.recipient if args.recipient is not None else None
+        send_newsletter(config, querystart, queryend, events, recipient)
     if args.send_telegram:
         telegram_id = config['telegram']['group_id'] if args.telegram_id is None else args.telegram_id
         send_telegram(config, telegram_id, message(events, querystart, queryend, mode='md'))
--- src/post_caldav_events/output/newsletter.py
+++ src/post_caldav_events/output/newsletter.py
@@ -3,11 +3,8 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from post_caldav_events.output.message import date, message
 
-def get_recipients():
-    with open('post_caldav_events/newsletter_recipients.txt', 'r') as recipients:
-        return recipients.readlines()
 
-def send_newsletter(config:dict, query_start, query_end, events:dict):
+def send_newsletter(config:dict, query_start, query_end, events:dict, recipient):
     with smtplib.SMTP_SSL(config['mail']['server'], config['mail']['smtp_port'] , context=ssl.create_default_context()) as smtp:
         smtp.ehlo()
         smtp.login(config['mail']['account'], config['mail']['password']) 
@@ -15,7 +12,11 @@ def send_newsletter(config:dict, query_start, query_end, events:dict):
         mail["Subject"] = f"{config['mail']['newsletter']['subject']} {date(query_start)} - {date(query_end)}"
         mail["From"] = config['mail']['sender']
         mail.attach(MIMEText(message(events, query_start, query_end, mode='html'), "html"))
-        for recipient in get_recipients():
+        if recipient is None:
+            for recipient in config['mail']['newsletter']['recipients']:
+                mail["To"] = recipient['mail']
+                smtp.sendmail(config['mail']['sender'], recipient['mail'], mail.as_string())
+        else:
             mail["To"] = recipient
             smtp.sendmail(config['mail']['sender'], recipient, mail.as_string())
         smtp.quit()
