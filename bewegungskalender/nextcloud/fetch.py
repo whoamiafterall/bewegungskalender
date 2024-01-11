2f3fef64eac1108611a2dfafa39934de0c3c8eb2
Whoami <aberallesigut@riseup.net>
2024-01-11 15:34:18 +0100

    more logging

--- bewegungskalender/nextcloud/fetch.py
+++ bewegungskalender/nextcloud/fetch.py
@@ -1,10 +1,12 @@
 import caldav
 import icalendar
+import logging
 from collections import namedtuple
 from bewegungskalender.helper.datetime import check_datetime, days, to_timezone, date, fix_midnight
     
 def connect_davclient(config:dict):
-    return caldav.DAVClient(url=config['caldav']['url'], username=config['caldav']['username'], password=config['caldav']['password'])    
+    return caldav.DAVClient(url=config['caldav']['url'], username=config['caldav']['username'], password=config['caldav']['password'])
+    
 
 def get_calendar_name(calendar:caldav.Calendar):
     return calendar.get_properties([caldav.dav.DisplayName()])['{DAV:}displayname']
@@ -26,7 +28,7 @@ def parse_event_data(event):
 
 def search(calendar:caldav.Calendar, querystart, queryend):
     events = []
-    for cal_data in calendar.search(querystart, queryend, event=True, expand=True):
+    for cal_data in calendar.search(start=querystart, end=queryend, event=True, expand=True):
         ical_data = icalendar.Event.from_ical(cal_data.data)
         for component in ical_data.walk():
             if component.name == "VEVENT":
@@ -40,13 +42,15 @@ def fetch_events(config: dict, querystart: int, queryend: int, data = []) -> dic
     Returns a dict with an Array of Events mapped to each calendars name (string).
     """
     try:
+        logging.debug('Connecting to DAVClient...')
         davclient = connect_davclient(config) 
     except ConnectionError:
-        print("Connection to Nextcloud failed.")
+        logging.error("Connection to Nextcloud failed.", ConnectionError)
         exit()
     for configline in config['calendars']:
         calendar = namedtuple("calendar", ["emoji", "name", "events"])
         url = davclient.calendar(url=configline['calendar']['url'])
+        logging.debug('Searching calendars for events...')
         events = search(url, querystart, queryend)
         calendar.name = get_calendar_name(url)
         calendar.events = (sorted([e for e in events], key=lambda d:d['start']))
