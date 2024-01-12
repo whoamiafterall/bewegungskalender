eee6e90ad6532cab7e149601b4149594f850a041
Whoami <aberallesigut@riseup.net>
2024-01-12 16:44:20 +0100

    added logging to telegram.py

--- bewegungskalender/output/telegram.py
+++ bewegungskalender/output/telegram.py
@@ -1,3 +1,5 @@
+import logging
+
 def bot(config):
     """
     get a Telegram Bot object with token from config
@@ -10,6 +12,7 @@ def get_telegram_updates(config):
     """
     telegram helper method to retrieve group_ids of groups that the bot joined
     """
+    logging.debug('Getting telegram updates...')
     updates = bot(config).get_updates()
     return "\n".join([str(u) for u in updates])
 
@@ -18,5 +21,5 @@ def send_telegram(config, telegram_id, message):
     if len(message) < 9500:
         bot(config).send_message(text=message, chat_id=telegram_id, parse_mode=ParseMode.MARKDOWN_V2, disable_web_page_preview=True)
     else:
-        print (len(message))
+        logging.error('Could not send message to telegram, message too long.', len(message))
     return
