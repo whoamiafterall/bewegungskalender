6d95d2d78c8151aaa0d0561fddcbd3cd2b9bb270
Whoami <aberallesigut@riseup.net>
2024-01-12 16:41:59 +0100

    update init.py imports

--- bewegungskalender/__init__.py
+++ bewegungskalender/__init__.py
@@ -1,3 +1,7 @@
+import argparse
+import yaml
+import locale
+import logging
 from bewegungskalender.helper.datetime import set_timezone, today, days
 from bewegungskalender.helper.formatting import Format
 from bewegungskalender.input.form import update_events
@@ -7,11 +11,12 @@ from bewegungskalender.output.telegram import send_telegram, get_telegram_update
 from bewegungskalender.output.umap import createMapData
 from bewegungskalender.output.mail import send_mail
 
-__all__ = ([set_timezone, today, days] +
-           [Format] +
-           [update_events] +
-           [fetch_events] +
-           [message] +
-           [send_telegram, get_telegram_updates] +
-           [createMapData] +
-           [send_mail])
+__all__ =   ([argparse, yaml, locale, logging]
+            [set_timezone, today, days] +
+            [Format] +
+            [update_events] +
+            [fetch_events] +
+            [message] +
+            [send_telegram, get_telegram_updates] +
+            [createMapData] +
+            [send_mail])
