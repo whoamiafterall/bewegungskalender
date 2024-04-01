da821cde313207e2c56a608073cc4c52822cb99b
Whoami <aberallesigut@riseup.net>
2024-04-01 19:15:48 +0200

    move argparse to cli.py to make main more readable

---
+++ bewegungskalender/helper/cli.py
@@ -0,0 +1,60 @@
+import argparse
+from ast import arg
+import logging
+from bewegungskalender.helper.formatting import Format
+
+# Get Arguments from Commandline 
+def get_args ():
+    argparser = argparse.ArgumentParser(prog="bewegungskalender", description='Fetch CalDav Events from a Nextcloud and send a Message to Telegram.')
+    argparser.add_argument("-c", "--config", dest='config_file', help='specify path to config file, defaults to config.yml')
+    argparser.add_argument("-g", "--get-telegram-updates", dest='get_telegram_updates', help='get telegram id of channel', action='store_true')
+    argparser.add_argument("-l", "--log", dest='log_level', help='set the log level to debug, info, warn or error - defaults to info', choices=['debug','info','warn','error'], action='store')
+    argparser.add_argument("-m", "--map", dest='update_map', help='create MapData in geojson from loaction entries of events', action='store_true')
+    argparser.add_argument("-n", "--newsletter", dest='send_mail', help='send email-to recipients specified or from config', action='store_true')
+    argparser.add_argument("-p", "--print", dest='print', help='print message to stdout and select format from html, markdown or plain-text - defaults to txt', choices=['html','md','txt'], action='store')
+    argparser.add_argument("-qs", "--query-start", dest='query_start', type=int, help='starting day to query events from CalDav server, 0 means today, 1 tomorrow')
+    argparser.add_argument("-qe", "--query-end", dest='query_end', type=int, help='number of days to query events from CalDav server, starting from query-start')
+    argparser.add_argument("-r", "--recipients", dest='recipient', help='override mail recipients from config')
+    argparser.add_argument("-t", "--telegram", dest='telegram', help='send message to telegram - choose production or test_channel specified in config', choices=['prod', 'test'])
+    argparser.add_argument("-toot", "--mastodon", dest='send_mastodon', help='send toot to mastodon', action='store_true')
+    argparser.add_argument("-u", "--update-events", dest='update_events', help='check Mailbox for new events and add them to calendar', action='store_true')
+    argparser.set_defaults(config_file="config.yml", print='txt', log_level='info', query_start=1, query_end=1, telegram=None)
+    args = argparser.parse_args()
+    return args
+
+# Handle --log
+def set_log_level(args):
+    if args.log_level == 'debug':
+        return logging.DEBUG
+    elif args.log_level == 'warn':  
+        return logging.WARN
+    elif args.log_level == 'error':  
+        return logging.ERROR
+    else: 
+        return logging.INFO
+
+# Handle --print and set Format to HTML, MarkDown or TXT
+def set_print_format(args):
+    if args.print == 'html':
+        format = Format.HTML     
+    elif args.print == 'md':
+        format = Format.MD
+    else:
+        format = Format.TXT
+    return format
+
+#  Handle --telegram and set channel to production if specified
+def set_telegram_channel(args, config):
+    if args.telegram == 'prod':
+        channel = config['telegram']['production'] 
+    else:
+        channel = config['telegram']['test']
+    return format
+
+ # Handle --recipients and set recipients if specified
+def set_mail_recipients(args, config):
+    if args.recipient is not None:
+        recipients = args.recipient 
+    else: 
+        config['mail']['newsletter']['recipients']
+    return recipients
--- bewegungskalender/main.py
+++ bewegungskalender/main.py
@@ -1,103 +1,74 @@
-"""
-main executable
-"""
-import argparse
 import yaml
 import locale
 import logging
+from collections import namedtuple
+from bewegungskalender.helper.cli import get_args, set_log_level, set_mail_recipients, set_print_format, set_telegram_channel
 from bewegungskalender.helper.datetime import set_timezone, today, days
 from bewegungskalender.helper.formatting import Format
-from bewegungskalender.input.form import update_events
-from bewegungskalender.nextcloud.fetch import fetch_events
+from bewegungskalender.input.wpforms import update_events
+from bewegungskalender.server.dav import connect_davclient, search_events, search_events
 from bewegungskalender.output.message import message
 from bewegungskalender.output.telegram import send_telegram, get_telegram_updates
 from bewegungskalender.output.umap import createMapData
 from bewegungskalender.output.mail import send_mail
 #from bewegungskalender.output.mastodon import login
 
-# Get Arguments from Commandline 
-def get_args ():
-    argparser = argparse.ArgumentParser(description='Fetch CalDav Events from a Nextcloud and send a Message to Telegram.')
-    argparser.add_argument("-c", "--config", dest='config_file', help='specify path to config file, defaults to config.yml')
-    argparser.add_argument("-g", "--get-telegram-updates", dest='get_telegram_updates', help='get telegram id of channel', action='store_true')
-    argparser.add_argument("-m", "--map", dest='update_map', help='create MapData in geojson from loaction entries of events', action='store_true')
-    argparser.add_argument("-n", "--newsletter", dest='send_mail', help='send email-to recipients specified or from config', action='store_true')
-    argparser.add_argument("-p", "--print", dest='print', help='print message to stdout and select format from html, markdown or plain-text - for debugging', choices=['html','md','txt'], action='store')
-    argparser.add_argument("-qs", "--query-start", dest='query_start', type=int, help='starting day to query events from CalDav server, 0/None means today')
-    argparser.add_argument("-qe", "--query-end", dest='query_end', type=int, help='number of days to query events from CalDav server, starting from query-start')
-    argparser.add_argument("-r", "--recipients", dest='recipient', help='override mail recipients from config')
-    argparser.add_argument("-t", "--telegram", dest='telegram', help='send message to telegram - choose production or test_channel specified in config', choices=['prod', 'test'])
-    argparser.add_argument("-toot", "--mastodon", dest='send_mastodon', help='send toot to mastodon', action='store_true')
-    argparser.add_argument("-u", "--update-events", dest='update_events', help='check Mailbox for new events and add them to calendar', action='store_true')
-    argparser.set_defaults(config_file="config.yml", print=None, query_start=1, query_end=1, telegram=None)
-    args = argparser.parse_args()
-    return args
-
 # Main Function if run as standalone program
 def main(events = {}):
-    logging.basicConfig(level=logging.DEBUG)
-    logging.debug('Started bewegungskalender.main...')
-    logging.debug('Parsing arguments from command-line...')
-    args = get_args() # get Arguments from Command-Line
-    try: # get Config Dictionary from YAML file
+    # get Arguments from Command-Line and set log-level
+    args = get_args()
+    logging.basicConfig(level=set_log_level(args))
+    logging.info(f"Running with the following args: {args}")
+    
+    # get Config from yml file
+    logging.debug('Loading config file...')
+    try:
         with open(args.config_file, 'r') as f: 
-            logging.debug('Loading config file...')
             config:dict = yaml.load(f, Loader=yaml.FullLoader) 
     except FileNotFoundError:
         logging.exception('Config File not Found:', args.config_file); exit()
+        
+    # Set Timezone and Locale
     logging.debug('Setting timezone and locale...')
     locale.setlocale(locale.LC_TIME, config['format']['locale']) 
     set_timezone(config)
+    
+    # Telegram Config
     if args.get_telegram_updates: 
-        #get id of telegram channel where the bot has been added
-        logging.debug('Getting telegram channel id...')
+        logging.info('Getting telegram channel id...')
         print(get_telegram_updates(config)); exit() 
+    
+    # Input Section
+    ### WPForms Input
     if args.update_events: 
-        #update events from Form Mails sent by WP Forms Lite
-        logging.debug('Updating Events by scanning E-Mails...')
-        update_events(config)    
-    #Search nextcloud calendars for events in a specific range
+        logging.info('Updating Events by scanning E-Mails from WPForms Lite...')
+        update_events(config, connect_davclient(config))    
+    
+    # Server Section    
+    ### Fetch Events from CalDav-Server
     start = today() + days(args.query_start); 
-    end = start + days(args.query_end) 
-    logging.debug('Fetching Events from Nextcloud server...')
-    events = fetch_events(config, start, end)
+    stop = start + days(args.query_end)
+    data = search_events(config, start, stop)
+    
+    # Output Section
+    ### UMap Output
     if args.update_map: 
-        #create GeoJSON Files
-        logging.debug('Creating Map Data and writing to GeoJSON Files...')
+        logging.info('Creating Map Data and writing to GeoJSON Files...')
         createMapData(events)
+    ### Print Output
     if args.print: 
-        #print message to stdout for debugging or copying
-        logging.debug('Printing Message to stdout...')
-        print(f"This is the message in {args.print} Format: \n")
-        #set Format to cli-argument (HTML, MarkDown or TXT)
-        if args.print == 'html':
-            format = Format.HTML     
-        elif args.print == 'md':
-            format = Format.MD
-        else:
-            format = Format.TXT
-        print(message(config, events, start, end, format))
+        logging.info(f"Printing message in {args.print} Format: \n")
+        print(message(config, data, start, stop, set_print_format(args)))
+    ### Mail Output
     if args.send_mail: 
-        #send message as mail newsletter
-        logging.debug('Sending Message per Mail...')
-        #set recipients to cli-arguments or config
-        if args.recipient is not None:
-            recipients = args.recipient 
-        else: 
-            config['mail']['newsletter']['recipients']
-        send_mail(config, start, end, events, recipients, Format.HTML)
+        logging.info('Sending Message per Mail...')
+        send_mail(config, start, stop, data, set_mail_recipients, Format.HTML)
+    ### Telegram Output
     if args.telegram: 
-        #send message to telegram 
-        logging.debug('Sending Message to Telegram Channel...')
-        #set telegram channel to production or test
-        if args.telegram == 'prod':
-            channel = config['telegram']['production'] 
-        else:
-            config['telegram']['test']
-        text = message(config, events, start, end, Format.MD) 
-        send_telegram(config, channel, text)
-        print(f"Succesfully sent message to {args.telegram}: {channel}!")
-    logging.debug('Finished all Tasks - Quitting.')
+        logging.info('Sending Message to Telegram Channel...')
+        send_telegram(config, set_telegram_channel(args, config), message(config, data, start, stop, Format.MD))
+        logging.info(f"Succesfully sent message to Telegram Channel {args.telegram}!")
+    logging.info('Finished all Tasks - Quitting.')
     exit()
     
 # send mastodon newsletter
