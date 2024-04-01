9fd41fe6b9ea0b5299aac50b3ceeb2dc28560699
Whoami <aberallesigut@riseup.net>
2024-04-01 13:47:54 +0200

    removed now(), not in use

--- bewegungskalender/helper/datetime.py
+++ bewegungskalender/helper/datetime.py
@@ -10,12 +10,6 @@ def set_timezone(config:dict):
 def today() -> datetime.date:
     return datetime.datetime.now().date()
 
-def now(type: [str, datetime]):
-    if type == str:
-        return datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-    if type == datetime:
-        return datetime.datetime.now()
-
 def to_datetime(date:str, config) -> datetime.datetime:
     "returns a datetime.datetime Object from a String using format specified in config."
     return datetime.datetime.strptime(date, config['mail']['input']['date_format'])
