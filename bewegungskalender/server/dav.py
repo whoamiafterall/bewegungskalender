f98790f9fb7fc41e2dad5264fb171b8541f42c56
Whoami <aberallesigut@riseup.net>
2024-04-02 12:30:40 +0200

    upgraded calobject.date_search() to calendar.search() because of deprecation

--- bewegungskalender/server/dav.py
+++ bewegungskalender/server/dav.py
@@ -8,7 +8,8 @@ def connect_davclient(config:dict):
     # Try to connect to CalDAV-Server
     logging.info("Connecting to CalDav-Server using credentials from config...")
     try:
-        return caldav.DAVClient(url=config['caldav']['url'], username=config['caldav']['username'], password=config['caldav']['password'])    
+        with caldav.DAVClient(url=config['caldav']['url'], username=config['caldav']['username'], password=config['caldav']['password']) as client:
+            return client    
     except ConnectionError:
         logging.exception("Connection to CalDav-Server failed.")
         exit()
@@ -26,8 +27,13 @@ def search_events(config: dict, start: int, stop: int) :
         calobject = davclient.calendar(url=url)
         
         # Search for Events this Calendar in the given timeframe and add them to a list
+        search_filter = {
+            'start': start,
+            'end': stop,
+        }
+
         events = []
-        for cal_data in calobject.date_search(start, stop, "VEVENT"):
+        for cal_data in calobject.search(**search_filter, event=True, expand=True, sort_keys=['dtstart', 'summary']):
             for component in icalendar.Event.from_ical(cal_data.data).walk():
                 if component.name == "VEVENT":
                     events.append(parse_event(component))
@@ -38,7 +44,7 @@ def search_events(config: dict, start: int, stop: int) :
             calendar.name = calobject.get_properties([caldav.dav.DisplayName()])['{DAV:}displayname']
         except ConnectionError:
             calendar.name = url
-        calendar.events = sorted(events, key=lambda e: e.start)
+        calendar.events = events
         calendar.emoji = configline['calendar']['emoji']
         logging.info(f"Successfully parsed {len(calendar.events)} events from {calendar.name}!")
         data.append(calendar)
