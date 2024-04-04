f0d39367e208d80ffc78b4f197f3525d0ca6527a
Whoami <aberallesigut@riseup.net>
2024-04-04 23:50:38 +0200

    updated scripts to fix ssh problem (ssh-agent with ssh-key not spawned automatically)

--- scripts/telegram_and_newsletter.sh
+++ scripts/telegram_and_newsletter.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+source ~/.bash_profile
+ssh-add ~/.ssh/bewegungskalender_rsa
 cd ~/bewegungskalender2telegram/
 source ~/bewegungskalender2telegram/.venv/bin/activate
 python3 -m bewegungskalender.main -t prod -n
+deactivate
--- scripts/update_events_map_and_message.sh
+++ scripts/update_events_map_and_message.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+source ~/.bash_profile
+ssh-add ~/.ssh/bewegungskalender_rsa
 cd ~/bewegungskalender2telegram/
 source ~/bewegungskalender2telegram/.venv/bin/activate
 python3 -m bewegungskalender.main -u -m -qe 90
-cd ~/bewegungskalender2telegram/scripts
-./edit_last_telegram_message.sh
+python3 -m bewegungskalender.main -e test
+deactivate
