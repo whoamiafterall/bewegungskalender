b43dbaf799d67bff3bc539d5f96a8fa2838c0a5b
Whoami <aberallesigut@riseup.net>
2024-09-04 21:17:58 +0200

    added form

--- bewegungskalender/ui/main_page.py
+++ bewegungskalender/ui/main_page.py
@@ -9,6 +9,7 @@ from bewegungskalender.ui.helper.map import configure_map
 #nice gui support async function for page loading
 @ui.page('/')
 async def main_page():
+    ui.query('.nicegui-content').classes('p-0') # remove default padding from site
     ui.dark_mode(True) # Set dark mode
     ui.colors(primary='#1c2329', secondary='white')
     
@@ -17,18 +18,18 @@ async def main_page():
     with ui.header().classes('fixed p-0 m-0') as header:
         with ui.tabs().classes('w-full') as tabs:
             # Create one Tab for showing Help
-            if config['help']['display']:
-                help = ui.tab(name='help', label=config['help']['label'], icon='home')
+            help = ui.tab(name='help', label=config['help']['label'], icon='home')
             # Create one Tab for showing All Events in a List
-            if config['all_events']['display']:
-                all_events = ui.tab(name='all_events', label=config['all_events']['label'])
+            all_events = ui.tab(name='all_events', label=config['all_events']['label'], icon='calendar_month')
             # Create one Tab for each Category (Calendar)
           #  for calendar in data:
            #     if calendar.events != []:
            #         ui.tab(calendar.name)   
             # Create one Tab for the Map
-            if config['map']['display']:
-                ui.tab(config['map']['label'], icon='map')
+            ui.tab(config['map']['label'], icon='map')
+            # Create one tab for the Form
+            form = ui.tab(name='form', label=config['form']['label'], icon='edit_calendar')
+                
     
     # Create Tab Panels (what is shown when Tab is selected)
     debug('Creating Tab Panels (Content)...')
@@ -50,22 +51,27 @@ async def main_page():
             debug('Creating the List showing All Events...')
             with ui.tab_panel(all_events).classes('p-0 m-0'):
                 ui.html(config['all_events']['iframe']).classes('w-screen h-screen p-0 m-0')
-        
-            # Create One List View for each Category (Calendar) #TODO #FIXME
+                        
+            # Create Form View
+            debug('Creating the Form to enter a new event...')
+            with ui.tab_panel(form).classes('p-0 m-0'):
+                ui.html(config['form']['iframe']).classes('w-screen h-screen p-0 m-0')
+            
+            # Create Map View
+            debug('Creating the Map to show events...')
+            with ui.tab_panel(config['map']['label']).classes('p-0 m-0'):                      
+                # new map with center set to center of germany
+                map = ui.leaflet(center=(config['map']['center']['lat'], config['map']['center']['lon']), zoom=config['map']['zoom']).classes('w-screen h-screen p-0 m-0')
+                map = await configure_map(map)
+            
+                
+                 # Create One List View for each Category (Calendar) #TODO #FIXME
           #  for calendar in data:
            #     debug(f"Adding List View of {calendar.name} to UI...")
             #    if calendar.events != []:
              #       with ui.tab_panel(calendar.name):
               #          ui.html(message.html)   
-                        
-            # Create Map View
-            with ui.tab_panel(config['map']['label']).classes('p-0 m-0'):                      
-                # add static files
-                # new map with center set to center of germany
-                map = ui.leaflet(center=(config['map']['center']['lat'], config['map']['center']['lon']), zoom=config['map']['zoom']).classes('w-screen h-screen p-0 m-0')
-                map = await configure_map(map)
 
-    ui.query('.nicegui-content').classes('p-0') # remove default padding from site
    # storage_secret = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(32))
 
 #seperate start_ui out from main page
@@ -73,5 +79,6 @@ def start_ui():
     debug('Finished. Starting UI...')
     ui.run(title=config['title'], favicon=config['favicon'], port=config['port']) #storage_secret=storage_secret)
     debug('Successfully started UI.')
+    # add static files
     app.add_static_files(config['assets']['url_path'], config['assets']['local_dir'])  
 
