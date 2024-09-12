ec3a8eb47e327b5b30cea012c4b68377e839ec28
Whoami <aberallesigut@riseup.net>
2024-09-12 23:04:57 +0200

    updated init.py

--- bewegungskalender/__init__.py
+++ bewegungskalender/__init__.py
@@ -1,26 +1,33 @@
-import argparse
-import yaml
-import locale
-import logging
+# external imports
+import asyncio
+from typing import NamedTuple
+from locale import setlocale, LC_TIME
+
+# internal imports
+from bewegungskalender.helper.cli import LOGLEVEL, FORMAT, ARGS, START, END #set_telegram_channel
+from bewegungskalender.helper.logger import LOG
+from bewegungskalender.helper.config import config
 from bewegungskalender.helper.datetime import set_timezone
-from bewegungskalender.helper.formatting import Format
-from bewegungskalender.helper.cli import get_args, set_telegram_channel, set_print_format
-from bewegungskalender.input.wpforms import update_wpform
 from bewegungskalender.input.nextcloud_forms import update_ncform
-from bewegungskalender.server.calDAV import search_events
-from bewegungskalender.output.message import get_message
-from bewegungskalender.output.telegram import Channel, send_or_edit_telegram, get_telegram_updates
+from bewegungskalender.input.wpforms import update_wpform
+from bewegungskalender.server.calDAV import connect_davclient, search_events
+from bewegungskalender.output.message import MultiFormatMessage, create_message
+from bewegungskalender.output.telegram import send_or_edit_telegram # get_telegram_updates
 from bewegungskalender.output.umap import createMapData
 from bewegungskalender.output.mailnewsletter import send_mail
+from bewegungskalender.ui.main_page import start_ui
 
-__all__ =   ([argparse, yaml, locale, logging] +
+__all__ =   ([asyncio, NamedTuple, setlocale, LC_TIME] +
             [set_timezone] +
-            [Format] +
-            [get_args, set_print_format, set_telegram_channel] +
+            [config] +
+            [LOGLEVEL, FORMAT, ARGS, START, END] +
             [update_wpform] +
             [update_ncform] +
-            [search_events] +
-            [get_message] +
-            [Channel, send_or_edit_telegram, get_telegram_updates] +
+            [MultiFormatMessage, create_message] +
+            [search_events, connect_davclient] +
+            [LOG] +
+            [send_or_edit_telegram] +
             [createMapData] +
+            [start_ui] +
             [send_mail])
+            
