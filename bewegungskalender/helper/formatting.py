d543e1c9b0b6294e68c74e99fdddfb5f57d91b72
Whoami <aberallesigut@riseup.net>
2024-09-12 23:00:16 +0200

    added get method to Class MultiFormatMessage. Added Style Class to formatting.py. This could be useful when templating. Fixed eventtime error through rename of "date" to "date_str".

--- bewegungskalender/helper/formatting.py
+++ bewegungskalender/helper/formatting.py
@@ -1,58 +1,102 @@
 from datetime import datetime
 from enum import Enum
-import logging
 import os
 import re
 from typing import Tuple
-from bewegungskalender.helper.datetime import time, date
+from bewegungskalender.helper.datetime import date_str, time
+from bewegungskalender.helper.logger import LOG
 
-class Format(Enum):
+class Format(Enum): # to make dot notation available
     HTML = 'html'
     MD = 'markdown'
     TXT = 'txt'
+    
+class Style(Enum):
+    BOLD = 'bold'
+    ITALIC = 'italic'
+    CODE = 'code'
+    STRIKETHROUGH = 'strikethrough'
+    UNDERLINE = 'underline'
+    HIGHLIGHT = 'highlight'
 
 # generic functions for handling different text formatting
+def newline() -> str:
+    """Returns a newline statement ('\n'). Useful to reuse when templating content.
 
-def newline():
+    Returns:
+        str: '\n' => the newline statement
+    """    
     return "\n"
 
-def bold(text:str, mode: Format) -> str:
-    return "<b> " + text + " </b>" if mode == Format.HTML else ("*" + text + "*")
+def style(text:str, format: Format, style:Style) -> str:
+    """Styles the given text in the given format with the given style. 
 
-def italic(text:str, mode: Format) -> str:
-    return "<i> " + text + " </i>" if mode == Format.HTML else ("_" + text + "_")
+    Args:
+        text (str): The text to be styled.
+        format (Format): The format to be used. See Format Class.
+        style (Style): The style to be used. See Style Class.
+
+    Returns:
+        str: The styled **text** in the given format with the given style.
+    """    
+    if format == Format.TXT:
+        return
+    match style:
+        case Style.BOLD:
+            return "<b> " + text + " </b>" if format == Format.HTML else ("*" + text + "*")
+        case Style.ITALIC:
+            return "<i> " + text + " </i>" if format == Format.HTML else ("_" + text + "_")
+        case Style.CODE:
+            return "<code> " + text + " </code>" if format == Format.HTML else ("`" + text + "`")
+        case Style.STRIKETHROUGH:
+            return "<s> " + text + " </s>" if format == Format.HTML else ("~~" + text + "~~")
+        case Style.UNDERLINE:
+            return "<u> " + text + " </u>"
+        case Style.HIGHLIGHT:
+            return "<mark> " + text + " </mark>" if format == Format.HTML else ("==" + text + "==")
+               
+             
+def escape(text:str, charset:str = "?_–*[]()~`>#+-=|.!'{''}''" ) -> str:
+    """Escapes characters from a given charset in a given string with `\\\`'. <br>
+    The default charset escapes all relevant characters for **Markdown and HTML** formatting.
     
-def escape_markdown(text:str) -> str:
+    Args:
+        text (str): The text where characters should be escaped with `\\\`.
+        charset (str): The characters to be escaped. Defaults to: "?_–*[]()~`>#+-=|.!'{''}''" 
+
+    Returns:
+        str: The given text with the only change that the given characters are escaped with `\\\`.
+    """    
     if text is None:
         return ""
-    charset = "?_–*[]()~`>#+-=|.!'{''}''"
-    translate_dict = {c: "\\" + c for c in charset}
+    # Add "\\" in front of each character in the charset and return
+    translate_dict = {c: "\\" + c for c in charset} 
     return text.translate(str.maketrans(translate_dict))
 
 def search_link(summary:str, description:str) -> str:
     if  description is not None:
-        try: 
+        try: # 
             return re.search("(?P<url>https?://[^\s]+)", description).group("url") 
         except AttributeError: 
-            logging.warn(f"L: {summary}: No Link in: {description}")
+            LOG.warning(f"L: {summary}: No Link in: {description}")
             return None
     
 def md_link(text:str, url:str) -> str:
     if search_link(text, url) is not None:
-        return f" [{escape_markdown(text)}]({search_link(text, url)})"
+        return f" [{escape(text)}]({search_link(text, url)})"
     else:
-        return f" {escape_markdown(text)}"
+        return f" {escape(text)}"
 
-def match_and_add_recurring(event:Tuple, message:str, mode:Format) -> str:
+def match_and_add_recurring(event:Tuple, message:str, format:Format) -> str:
     # Prepare regex, the primary entry of the event and the date to add if its not the first occurence
-    if mode == Format.TXT:
+    if format == Format.TXT:
         regex = r'(\d{2}\.\d{2}\.\s)(\(\d{2}\:\d{2}\)\:\s)?' + f"({re.escape(event.summary)})"
         event_string = eventtime(event.start, event.end) + f" {event.summary}"
-        date_to_add = f"& {date(event.start)} "
-    elif mode == Format.MD or Format.HTML:
-        regex = r'(\d{2}\\\.\d{2}\\\.\s)(\\\(\d{2}\:\d{2}\\\)\:\s)?' + f"(\[{re.escape(escape_markdown(event.summary))}\])"
-        event_string =  escape_markdown(eventtime(event.start, event.end)) + md_link(event.summary, event.description)
-        date_to_add =  f"& {escape_markdown(date(event.start))} "
+        date_to_add = f"& {date_str(event.start)} "
+    elif format == Format.MD or Format.HTML:
+        regex = r'(\d{2}\\\.\d{2}\\\.\s)(\\\(\d{2}\:\d{2}\\\)\:\s)?' + f"(\[{re.escape(escape(event.summary))}\])"
+        event_string =  escape(eventtime(event.start, event.end)) + md_link(event.summary, event.description)
+        date_to_add =  f"& {date_str(event.start)} "
     
     match = re.search(regex, message) # Try to match the event in the existing message
     if match is None:                 # If it's the first occurence, add the primary entry of the event and return
@@ -72,8 +116,8 @@ def to_filename(text):
     return os.path.basename(text)
 
 def eventtime(start:datetime, end:datetime) -> str:
-    if time(start) == "(00:00)" and date(start) != date(end):
-        return f"{date(start)} - {date(end)}"
-    elif time(start) == "(00:00)" and date(start) == date(end):
-        return f"{date(start):}"
-    return f"{date(start)} {time(start)}:"
+    if time(start) == "(00:00)" and start.date() != end.date():
+        return f"{date_str(start)} - {date_str(end)}"
+    elif time(start) == "(00:00)" and start.date() == end.date():
+        return f"{date_str(start):}"
+    return f"{date_str(start)} {time(start)}:"
--- bewegungskalender/output/message.py
+++ bewegungskalender/output/message.py
@@ -1,38 +1,61 @@
 from datetime import date, datetime, time
-import logging
 from typing import Tuple
+from bewegungskalender.helper.cli import START, END
+from bewegungskalender.helper.logger import LOG
 from bewegungskalender.helper.datetime import weekday_date
-from bewegungskalender.helper.formatting import eventtime, match_and_add_recurring, md_link, escape_markdown, bold, Format
+from bewegungskalender.helper.formatting import Style, eventtime, match_and_add_recurring, md_link, escape, style, Format
 
 class MultiFormatMessage():
-    def __init__(self, start:date, end:date) -> None:
-        self.start = start
-        self.end = end
+    def __init__(self) -> None:
+        self.start = START
+        self.end = END
         self.html:str = ""
         self.markdown:str = ""
         self.txt:str = ""
         
+    # set Format from a given String
+    def get(self, format:str|Format) -> str:
+        """Get the message of this instance in the specified format.
+
+        Args:
+            format (str|Format): The format to be used as a **string** or of type **Format**.
+
+        Returns:
+            str: The message formatted in the specified format.
+        """    
+        match format:
+            case 'html' | Format.HTML:
+                return self.html     
+            case 'md' | Format.MD:
+                return self.markdown
+            case 'txt' | Format.TXT:
+                return self.txt
+            case _:
+                LOG.warning(f"Format: {format} does not exist. Choose: 'html', 'md' or 'txt'.")   
+                exit(1) 
+            
     def __str__(self) -> str:
         return f"Events from {self.start} to {self.end} in Txt Format: \n{self.txt}"
     
     def __repr__(self) -> str:
         return f"MultiFormatMessage:\nstart={self.start}\nend={self.end}\ntxt=\n{self.txt}"
-
+    
     def newline(self):
         self.txt += "\n"
         self.markdown += "\n"
         self.html += "\n"
         
-    def queryline(self) -> str: # Displayed as Head of the Message => bold info on the Timerange
-        self.txt += (f"Die Termine vom " + weekday_date(self.start) + " - " + weekday_date(self.end))
-        self.markdown += bold(escape_markdown((f"Die Termine vom " + weekday_date(self.start) + " - " + weekday_date(self.end))), Format.MD)
-        self.html += bold(escape_markdown((f"Die Termine vom " + weekday_date(self.start) + " - " + weekday_date(self.end))), Format.HTML)
+    def header(self) -> str: # Displayed as Head of the Message => style info on the Timerange
+        header = f"Die Termine vom " + weekday_date(self.start) + " - " + weekday_date(self.end)
+        self.txt += (header)
+        self.markdown += style(escape(header), Format.MD, Style.BOLD)
+        self.html += style(escape(header), Format.HTML, Style.BOLD)
    
-    # Create Titles out of Calendar Names (Categories) and Emojis - adds bold for HTML and Markdown.
+    # Create Titles out of Calendar Names (Categories) and Emojis - adds style for HTML and Markdown.
     def calendar_title(self, emoji: str, name: str) -> str: 
         self.txt += f"{emoji} {name}"
-        self.markdown += bold(f"{emoji} {escape_markdown(name)}", Format.MD)
-        self.html += bold(f"{emoji} {escape_markdown(name)}", Format.HTML)
+        self.markdown += style(f"{emoji} {escape(name)}", Format.MD, Style.BOLD)
+        self.html += style(f"{emoji} {escape(name)}", Format.HTML, Style.BOLD)
         
     # This should be refactored at some point, but i'm happy that it properly works for now.
     def add_recurring_event(self, event: Tuple) -> str:
@@ -42,24 +65,25 @@ class MultiFormatMessage():
      
     def add_event(self, event:Tuple):
         self.txt += eventtime(event.start, event.end) + f" {event.summary}"
-        self.markdown += escape_markdown(eventtime(event.start, event.end)) + md_link(event.summary, event.description)
-        self.html += escape_markdown(eventtime(event.start, event.end)) + md_link(event.summary, event.description)
+        self.markdown += escape(eventtime(event.start, event.end)) + md_link(event.summary, event.description)
+        self.html += escape(eventtime(event.start, event.end)) + md_link(event.summary, event.description)
         
     def footer(self, config:dict) -> str:  # Displayed as End of the Message (List of links)
-        self.txt += "🌐 Weiterführende Links: \n"
-        self.markdown += bold("🌐 Weiterführende Links: \n", Format.MD)
-        self.html += bold("🌐 Weiterführende Links: \n", Format.HTML)
+        footer_title:str =  "🌐 Weiterführende Links: \n"
+        self.txt += footer_title
+        self.markdown += style(escape(footer_title), Format.MD, Style.BOLD)
+        self.html += style(escape(footer_title), Format.HTML, Style.BOLD)
         for item in config['links']:
             self.txt += f"{item['link']['text']}: {item['link']['url']}\n"
-            self.markdown += md_link(escape_markdown(item['link']['text']), item['link']['url']) + "\n"
-            self.html += md_link(escape_markdown(item['link']['text']), item['link']['url']) + "\n"
+            self.markdown += md_link(escape(item['link']['text']), item['link']['url']) + "\n"
+            self.html += md_link(escape(item['link']['text']), item['link']['url']) + "\n"
 
-def get_message(config:dict, data:list, start: date, stop: date) -> MultiFormatMessage:
-    message = MultiFormatMessage(start, stop)
-    message.queryline()
+def create_message(config:dict, data:list) -> MultiFormatMessage:
+    message = MultiFormatMessage()
+    message.header()
     message.newline()
     for calendar in data:
-        logging.debug(f"Formatting and adding {calendar.name} to message...")
+        LOG.debug(f"Formatting and adding {calendar.name} to message...")
         if calendar.events != []:
             message.newline()
             message.calendar_title(calendar.emoji, calendar.name) 
@@ -72,5 +96,5 @@ def get_message(config:dict, data:list, start: date, stop: date) -> MultiFormatM
                     message.newline()
     message.newline()
     message.footer(config)
-    logging.info(f"Successfully formatted the message!")
+    LOG.info(f"Successfully formatted the message!")
     return message
