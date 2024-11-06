882ea54ea9b6a1ac3d63dd4b44aad026f5dc29ac
Whoami <aberallesigut@riseup.net>
2024-11-06 22:22:56 +0100

    Links are now dynamically loaded from a yaml file :) The UI is now a Single Page App with a Custom Router instead of Tabs. TODO: Fix Router so it routes URLs from AdressBar.

--- bewegungskalender/ui/functions.py
+++ bewegungskalender/ui/functions.py
@@ -1,5 +1,13 @@
 from contextlib import contextmanager
+from typing import Tuple
+
+import yaml
 from nicegui import ui
+from slugify import slugify
+
+from bewegungskalender.functions.file import safe_open_read
+from bewegungskalender.ui.router import Router
+
 
 @contextmanager
 def tab_panel(tab:str):
@@ -9,17 +17,36 @@ def tab_panel(tab:str):
 @contextmanager
 def container(classes:str = None):
     with (ui.card().tight().classes(
-            'container mx-auto min-h-full overflow-auto px-10 ' # Layout - Trailing White Space is important!
-            'pt-5 bg-black text-base font-light text-secondary') # Text
+            'container mx-auto min-h-full overflow-auto p-10 ' # Layout - Trailing White Space is important!
+            'bg-black text-base font-light text-secondary') # Text
     as card):
         card.classes(classes) # Add Custom Classes
         yield card
 
 @contextmanager
-def my_button_card(text, on_click):
-    with ui.button('', on_click=on_click).classes('p-0 rounded'):
-        with ui.card().classes('bg-slate-50 hover:bg-slate-100 text-black') as card:
-            ui.label('UI elements before')
-            yield card  # The remaining code will be executed only after exiting the caller with block
-            ui.label(text).classes('w-full items-center text-black')
-            ui.label('UI elements after')
+def render_iframe(source:str): #TODO Add Input Validation - check for <iframe> and url
+    ui.html(source).classes('w-screen h-screen p-0 m-0')
+
+@contextmanager
+def loading(page_label:str, timeout:float = 0.7):
+    ui.notification(f"Lade {page_label}...", position='center', type='ongoing', spinner=True, timeout=timeout)
+
+@contextmanager
+def show_links(path):
+    with safe_open_read(path) as f:
+        links:dict[Tuple[str, str]] = yaml.load(f, Loader=yaml.FullLoader)
+        categories:list = []
+        for category in links.items():
+            with ui.column().classes('h-2/3 mb-5 lg:w-1/4 max-lg:w-1/2 max-sm:w-full'):
+                ui.markdown(f"##### {category[0]}")
+                categories.append(category[0])
+                with ui.scroll_area().classes('w-3/4 items-stretch').props("bar-style={width: '2px'}"):
+                    with ui.list().props('dense separator').classes():
+                        for item in category[1].items():
+                            with ui.item():
+                                ui.link(f"{item[0]}", item[1], new_tab=True).classes('font-medium')
+        with ui.column().classes('h-full lg:w-1/4 max-lg:w-1/2 max-sm:w-full'):
+            ui.markdown("##### Link Hinzufügen")
+            ui.select(options=categories, label="Kategorie wählen:", value=categories[0])
+            ui.input(label='Stadt/Name:')
+         #   ui.input(label='Link:', validation='') #TODO Finish the validation here
--- bewegungskalender/ui/main_page.py
+++ bewegungskalender/ui/main_page.py
@@ -1,93 +1,132 @@
 from nicegui import ui, app
-from bewegungskalender.functions.config import CONFIG
+from slugify import slugify
+
+from bewegungskalender.functions.config import CONFIG, UI_PORT, UI_FAVICON, UI_TITLE, MAP_ZOOM, MAP_CENTER_LAT, \
+    MAP_CENTER_LON, MENU_ITEMS
 from bewegungskalender.functions.logger import LOGGER
-from bewegungskalender.ui.functions import tab_panel, container
+from bewegungskalender.static.history import history
+from bewegungskalender.ui.functions import container, show_links, render_iframe, loading
 from bewegungskalender.ui.map import configure_map
+from bewegungskalender.ui.router import Router
 
 
 #nice gui support async function for page loading
 @ui.page('/')
+@ui.page('/{_:path}')
 async def main_page():
     ui.query('.nicegui-content').classes('p-0') # remove default padding from site
     ui.dark_mode(True) # Set dark mode
-    ui.colors(primary='#1c2329', secondary='white')
-    
-    # Create Tabs
-    LOGGER.debug('Creating Tabs...')
-    with ui.header().classes('fixed p-0 m-0') as header:
-        with ui.tabs().classes('w-full') as tabs:
-            home = ui.tab(name='main_page', label=CONFIG['main_page']['label'], icon='home')
-            calendar = ui.tab(name='all_events', label=CONFIG['all_events']['label'], icon='calendar_month')
-            # Create one Tab for each Category (Calendar)
-          #  for calendar in data:
-           #     if calendar.events != []:
-           #         ui.tab(calendar.name)
-            # Create one Tab for the Map
-            ui.tab(CONFIG['map']['label'], icon='map')
-            # Create one tab for the Form
-            form = ui.tab(name='form', label=CONFIG['form']['label'], icon='edit_calendar')
-            faq = ui.tab(name='legende', label=CONFIG['legende']['label'], icon='question_mark')
-            links = ui.tab(name='links', label=CONFIG['links']['label'], icon='link')
-    
-    # Create Tab Panels (what is shown when Tab is selected)
-    LOGGER.debug('Creating Tab Panels (Content)...')
+    ui.colors(primary='#1c2329', secondary='white', accent='green')
+    router = Router()
 
-    with ui.card().classes('w-screen h-dvh p-0'):
-        with ui.tab_panels(tabs, value=home).classes('w-full h-full fixed'):
+    # Create Calendar Page
+    @router.add('/')
+    def calendar():
+        LOGGER.debug(f"Creating the Calendar View using {MENU_ITEMS['calendar']['source']}")
+        loading(MENU_ITEMS['calendar']['label'])
+        render_iframe(MENU_ITEMS['calendar']['source'])
+        with ui.page_sticky(x_offset=18, y_offset=18):
+            ui.button(icon='filter_alt', on_click=lambda: right_drawer.toggle()).props(
+                'fab color=accent').classes("sm:hidden")
 
-            # Create one Tab for displaying Homepage
-            LOGGER.debug(f"Creating Help Panel with the content of {CONFIG['main_page']['path']}...")
-            with tab_panel(home):
-                    with container('w-2/3'):
-                        with open(CONFIG['main_page']['path'], 'r') as f: # open file
-                            ui.markdown(f.read())
+    # Create Map View
+    @router.add(slugify(f"/{str(MENU_ITEMS['map']['label'].lower())}"))
+    async def event_map():
+        loading(MENU_ITEMS['map']['label'])
+        LOGGER.debug('Creating the Map to show events...')
+        # new leaflet with center set to center of germany
+        with ui.leaflet(center=(MAP_CENTER_LAT, MAP_CENTER_LON), zoom=MAP_ZOOM).classes('w-full h-full') as leaflet:
+            with ui.page_sticky(x_offset=18, y_offset=18).classes('sm:hidden z-5000'): #TODO Fix (it's not shown for whatever reason)
+                ui.button(icon='filter_alt', on_click=lambda: right_drawer.toggle()).props(
+                    'fab color=accent')
+        await configure_map(leaflet)
 
-            # Create Calendar Page
-            LOGGER.debug('Creating the List showing All Events...')
-            with tab_panel(calendar):
-                ui.html(CONFIG['all_events']['iframe']).classes('w-screen h-screen p-0 m-0')
+    # Create Form Page
+    @router.add(slugify(f"/{str(MENU_ITEMS['form']['label'].lower())}"))
+    def form():
+        loading(MENU_ITEMS['form']['label'])
+        LOGGER.debug(f"Creating the Form using {MENU_ITEMS['form']['source']}.")
+        render_iframe(MENU_ITEMS['form']['source'])
 
-            # Create Form Page
-            LOGGER.debug('Creating the Form to enter a new event...')
-            with tab_panel(form):
-                ui.html(CONFIG['form']['iframe']).classes('w-screen h-screen p-0 m-0')
+    # Create About Page
+    @router.add(slugify(f"/{str(MENU_ITEMS['about']['label'].lower())}"))
+    def about():
+        LOGGER.debug(f"Creating About Panel with the content of {MENU_ITEMS['about']['source']}...")
+        with container('md:w-2/3'):
+            with open(MENU_ITEMS['about']['source'], 'r') as f:  # open file
+                ui.markdown(f.read())
+            history()
 
-            # Create FAQ Page
-            with tab_panel(faq):
-                with container():
-                    with open(CONFIG['legende']['path'], 'r') as f:  # open file
-                        ui.html(f.read()).classes('flex-none')
-                    with open(CONFIG['FAQ']['path'], 'r') as f:  # open file
-                        ui.markdown(f.read()).classes('flex-initial')
+    # Create FAQ Page
+    @router.add(slugify(f"/{str(MENU_ITEMS['FAQ']['label'].lower())}"))
+    def faq():
+        with container():
+            with open(MENU_ITEMS['FAQ']['source'], 'r') as f:  # open file
+                ui.html(f.read()).classes('flex-none')
 
-            # Create Links Page
-            with tab_panel(links):
-                with container():
-                    with open(CONFIG['links']['path'], 'r') as f:  # open file
-                        ui.markdown(f.read()).classes('w-full pt-5')
+    # Create Links Page
+    @router.add(slugify(f"/{str(MENU_ITEMS['links']['label'].lower())}"))
+    def links():
+        loading(MENU_ITEMS['links']['label'], 0.2)
+        with container('flex-row flex-wrap'):
+            show_links(MENU_ITEMS['links']['source'])
 
-            # Create Map View
-            LOGGER.debug('Creating the Map to show events...')
-            with ui.tab_panel(CONFIG['map']['label']).classes('p-0 m-0'):                      
-                # new map with center set to center of germany
-                map = ui.leaflet(center=(CONFIG['map']['center']['lat'], CONFIG['map']['center']['lon']), zoom=CONFIG['map']['zoom']).classes('w-screen h-screen p-0 m-0')
-                map = await configure_map(map)
+    with ui.right_drawer(value=False, fixed=True, elevated=True, top_corner=True).classes('p-0 m-0').props(
+            'width=auto') as right_drawer:
+        with container('px-0'):
+            with ui.column().classes('items-stretch w-full m-0 p-0'):
+                pass
 
+    with ui.header(elevated=True).classes('fixed h-50px m-0 px-3 py-2 items-center'):
+        with ui.button_group().props('flat'):
+            ui.button(on_click=lambda:left_drawer.toggle(), icon='menu').props('flat color=white').classes('lg:hidden')
+            ui.button(MENU_ITEMS['calendar']['label'], icon=MENU_ITEMS['calendar']['icon'],
+                      on_click=lambda: router.open(calendar))
+            ui.button(MENU_ITEMS['map']['label'], icon=MENU_ITEMS['map']['icon'],
+                      on_click=lambda: router.open(event_map))
+        ui.space().classes('max-sm:hidden')
+        with ui.row().classes('max-lg:hidden m-0 p-0'):
+            with ui.button_group().props('outline rounded'):
+                ui.button(MENU_ITEMS['form']['label'], icon=MENU_ITEMS['form']['icon'],
+                          on_click=lambda: router.open(form))
+                ui.button(MENU_ITEMS['about']['label'], icon=MENU_ITEMS['about']['icon'],
+                          on_click=lambda: router.open(about))
+                ui.button(MENU_ITEMS['FAQ']['label'], icon=MENU_ITEMS['FAQ']['icon'],
+                          on_click=lambda: router.open(faq))
+                ui.button(MENU_ITEMS['links']['label'], icon=MENU_ITEMS['links']['icon'],
+                          on_click=lambda: router.open(links))
+        ui.space().classes('max-sm:hidden')
+        ui.button("Filter", icon='filter_alt', on_click=lambda:right_drawer.toggle()).props('flat color=white').classes('max-sm:hidden')
 
+    with ui.left_drawer(value=False, fixed=True, elevated=True).classes('lg:hidden background-primary p-0 m-0').props('width=auto persistent=False') as left_drawer:
+        ui.space()
+        with ui.column(wrap=False, align_items='stretch').classes('w-full p-5'):
+            ui.button(MENU_ITEMS['form']['label'], icon=MENU_ITEMS['form']['icon'],
+                      on_click=lambda: router.open(form)).on_click(lambda:left_drawer.hide())
+            ui.button(MENU_ITEMS['about']['label'], icon=MENU_ITEMS['about']['icon'],
+                      on_click=lambda: router.open(about)).on_click(lambda:left_drawer.hide())
+            ui.button(MENU_ITEMS['FAQ']['label'], icon=MENU_ITEMS['FAQ']['icon'],
+                      on_click=lambda: router.open(faq)).on_click(lambda:left_drawer.hide())
+            ui.button(MENU_ITEMS['links']['label'], icon=MENU_ITEMS['links']['icon'],
+                      on_click=lambda:router.open(links)).on_click(lambda:left_drawer.hide())
+            ui.separator()
+            ui.button(icon='close', on_click=lambda:left_drawer.hide()).props('flat color=white align=center').classes('h-24px')
 
-                # Create One List View for each Category (Calendar) #TODO #FIXME
-          #  for calendar in data:
-           #     LOGGER.debug(f"Adding List View of {calendar.name} to UI...")
-            #    if calendar.events != []:
-             #       with ui.tab_panel(calendar.name):
-              #          ui.html(message.html)   
+    router.frame().classes('w-screen h-[calc(100vh-50px)] mt-50')
+
+    # Create One List View for each Category (Calendar) #TODO #FIXME
+    #  for calendar in data:
+    #     LOGGER.debug(f"Adding List View of {calendar.name} to UI...")
+    #    if calendar.events != []:
+    #       with ui.tab_panel(calendar.name):
+    #          ui.html(message.html)
 
    # storage_secret = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(32))
 
 #seperate start_ui out from main page
 def start_ui():
     LOGGER.debug('Finished. Starting UI...')
-    ui.run(title=CONFIG['title'], favicon=CONFIG['favicon'], port=CONFIG['port'], on_air=True) #storage_secret=storage_secret)
+    ui.run(title=UI_TITLE, favicon=UI_FAVICON, port=UI_PORT, on_air=True) #storage_secret=storage_secret)
     LOGGER.debug('Successfully started UI.')
     # add static files
     app.add_static_files(CONFIG['assets']['url_path'], CONFIG['assets']['local_dir'])  
