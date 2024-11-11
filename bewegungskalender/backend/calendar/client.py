cd54f809c2e7808c1dc4fce2ed199369d2e2c714
Whoami <aberallesigut@riseup.net>
2024-11-11 17:46:29 +0100

    Reworked CalDav-Client in client.py Added fields to event.py and category.py which may be useful for the filter function.

---
+++ bewegungskalender/backend/calendar/client.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from http.client import RemoteDisconnected
+
+from caldav.davclient import Calendar, DAVClient
+from caldav.elements.ical import CalendarColor
+from caldav.objects import CalendarObjectResource
+from requests.exceptions import ConnectionError
+
+from bewegungskalender.backend.calendar.event import Event
+from bewegungskalender.backend.io.cli import START, END
+from bewegungskalender.backend.io.config import CALENDARS, CALDAV_URL, CALDAV_PW, CALDAV_USR
+from bewegungskalender.libs.logger import LOGGER
+
+DAVCLIENT:DAVClient = DAVClient(url=CALDAV_URL, username=CALDAV_USR, password=CALDAV_PW)
+
+def get_calendars() -> list[Calendar]:
+    return DAVCLIENT.principal().calendars()
+
+def get_calendar(url):
+    LOGGER.debug(f"Getting Data from {url}...")
+    return DAVCLIENT.calendar(url=url)
+
+def get_calendar_name(cal:Calendar):
+    return cal.get_display_name()
+
+def get_calendar_color(cal:Calendar):
+    return cal.get_property(CalendarColor(),True)
+
+# All Interactions with the CalDav Server are in this file
+def get_upcoming_events(cal:Calendar) -> list[Event]:
+    LOGGER.info(f"Looking for Events between {datetime.date(START)} and {datetime.date(END)} in {len(CALENDARS)} calendars... ")
+    # Search for Events in this Calendar in the given timeframe and add them to a list
+    events:list[Event] = []
+    try:
+        cal_data:list[CalendarObjectResource] = cal.search(**{'start': START, 'end': END}, event=True, expand=True, sort_keys=['dtstart', 'summary'])
+    except ConnectionError or RemoteDisconnected:
+        LOGGER.exception("Couldn't get data from CalDAV-Server because of connection error. Please check your network connection and try again!")
+        return None and exit(1)
+    for ics in cal_data: # Basically a list of .ics links
+        events.append(Event.from_icalendar(ics))
+    return events
+
--- bewegungskalender/backend/io/nextcloud_forms.py
+++ bewegungskalender/backend/io/nextcloud_forms.py
@@ -1,24 +1,25 @@
 import csv
-import caldav
-import pytz
 import urllib.request
-from caldav import DAVClient
-from bewegungskalender.backend.io.config import NCFORM_URL, INPUT_CALENDAR, CALDAV_URL, CALDAV_USR, CALDAV_PW
 from datetime import datetime, timedelta
-from bewegungskalender.backend.io.cli import LAST_UPDATE
+
+import pytz
+
+from bewegungskalender.backend.calendar.client import DAVCLIENT
 from bewegungskalender.backend.calendar.event import Event
+from bewegungskalender.backend.io.cli import LAST_UPDATE
+from bewegungskalender.backend.io.config import NCFORM_URL, INPUT_CALENDAR
 from bewegungskalender.libs.logger import LOGGER
+
 
 def update_ncform():
     LOGGER.info('Checking Nextcloud Form Responses for new events...')
     LOGGER.debug("Downloading CSV-File with Responses to NC-Form...")
     file = urllib.request.urlretrieve(NCFORM_URL, "bewegungskalender/data/nextcloud_form.csv")[0]
-    davclient: DAVClient = caldav.DAVClient(url=CALDAV_URL, username=CALDAV_USR, password=CALDAV_PW)
-    calendar = davclient.calendar(url=INPUT_CALENDAR)
+    calendar = DAVCLIENT.calendar(url=INPUT_CALENDAR)
     with open(file) as f:
         for row in csv.DictReader(f):
             if datetime.fromisoformat(row['Timestamp']) > datetime.now(tz=pytz.utc) - timedelta(LAST_UPDATE):
-                event = Event.from_nextcloud_form(row).to_icalendar()
+                event = Event.from_nextcloud_form(row)
                 calendar.add_event(event.to_ical())
                 LOGGER.info(f"Successfully added {row['Start-Datum']}: {row['Titel']} to calender!")
             else:
