5485dff3cee614f337d3a784da762fc60062f028
Whoami <aberallesigut@riseup.net>
2024-11-28 22:43:04 +0100

    Bugfix: Markers and Popups are now loading again - but /assets is overriden by main_frame.py so icons are not loading. Also added logging for better debugging.

--- bewegungskalender/frontend/views/map.py
+++ bewegungskalender/frontend/views/map.py
@@ -1,4 +1,6 @@
 from nicegui import ui
+from nicegui.elements.leaflet import Leaflet
+from nicegui.elements.leaflet_layers import Marker
 from slugify import slugify
 from sqlmodel import select
 
@@ -13,9 +15,10 @@ from bewegungskalender.libs.logger import LOGGER
 
 
 # Create Map View
-@ROUTER.add(slugify(f"/{str(MENU['map']['label'].lower())}"))
+@ROUTER.add(slugify(f"{str(MENU['map']['label'].lower())}"))
 async def map_view():
     loading(MENU['map']['label'])
+    await ui.context.client.connected()
     LOGGER.debug('Creating the Map to show events...')
     # new leaflet with center set to center of germany
     with ui.leaflet(center=(MAP_CENTER_LAT, MAP_CENTER_LON), zoom=MAP_ZOOM).classes('w-full h-full') as leaflet:
@@ -39,18 +42,25 @@ async def map_view():
         )
 
         # get cached data
-        locations:list[Location] = db.exe(select(Location)).all()
+        locations:list[Location] = db.exe(select(Location).where(Location.lat is not None)).all()
+        LOGGER.info(f"Got {locations.__len__()} to display...")
 
-        # wait for leaflet to be intialized
+        # wait for leaflet to be initialized
+        LOGGER.debug("Waiting for Leaflet to be initialized...")
         await leaflet.initialized()
 
         # loop trough Locations
         for location in locations:
             marker = leaflet.marker(latlng=(location.lat, location.lon))
+            LOGGER.info(f"Created marker for {location.event.summary}.")
+
             # set icon
             await marker.run_method(':setIcon', f"L.icon({{iconUrl: '{location.event.category.map_marker}',iconSize: [60,60],iconAnchor:[30, 60],popupAnchor:[0,-60]}})")
 
             # use template html file and replace variables TODO: use a proper templating language like Jinja? (Didn't want to setup a templating environment just for one file though)
             # it might also a be an option to be generate all the html popups and store them as properties themselves also.
             context = {"summary": location.event.summary, "event_time": event_time(location.event.start, location.event.end), "location": location.name, "link": location.event.link}
-            await leaflet.run_layer_method(marker.id, 'bindPopup', render_map_template(context), timeout=5.0)
+            bind_popup(leaflet, marker, context)
+
+def bind_popup(leaflet:Leaflet, marker:Marker, context:dict[str, str | None]):
+    leaflet.run_layer_method(marker.id, 'bindPopup', render_map_template(context), timeout=5.0)
