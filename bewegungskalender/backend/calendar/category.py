1f4cde84e09e9e778ed99629f6f344b0b656a6ce
Nina <nina.e@systemli.org>
2024-12-10 13:30:31 +0100

    added Full_Database and Database_Sync Flags and working on syncing from caldav

--- bewegungskalender/backend/calendar/category.py
+++ bewegungskalender/backend/calendar/category.py
@@ -3,11 +3,11 @@ import sys
 import icalendar
 from caldav import SynchronizableCalendarObjectCollection
 from caldav.objects import CalendarObjectResource
-from nicegui.html import select
-from sqlmodel import SQLModel, Relationship, Field
+from sqlmodel import SQLModel, Relationship, Field, select
+from starlette.config import undefined
 
 from bewegungskalender.backend.calendar.client import get_calendar_by_url, get_calendar_name, get_calendar_color, \
-	sync_all_events, get_sync_token, get_all_events
+	sync_all_events, get_sync_token, get_all_events, get_upcoming_events
 from bewegungskalender.backend.calendar.event import Event
 from bewegungskalender.backend.io import db
 from bewegungskalender.libs.logger import LOGGER
@@ -28,7 +28,7 @@ class Category(SQLModel, table=True):
 	                                   cascade_delete=True)
 	
 	@classmethod
-	def create(cls, configline:dict):
+	def create(cls, full_db:bool, configline:dict):
 		cal = get_calendar_by_url(configline['calendar']['internal'])
 		category = Category(
 			name = get_calendar_name(cal),
@@ -39,8 +39,10 @@ class Category(SQLModel, table=True):
 			sync_token = str(get_sync_token(cal)),
 			map_marker = configline['calendar']['map_marker'],
 		)
-		category._update_events(get_all_events(cal))
-		#category._update_events(get_upcoming_events(cal)) # Deprecated, was used to only get a certain range
+		if full_db is False:
+			category._update_events(get_upcoming_events(cal))
+		else:
+			category._update_events(get_all_events(cal))
 		LOGGER.info(f"Found {len(category.events)} events in {category.name}!\nSaving to database...")
 		with db.session() as sess:
 			sess.add(category)
@@ -49,22 +51,37 @@ class Category(SQLModel, table=True):
 	def sync(self):
 		cal = get_calendar_by_url(self.internal)
 		updated_events = sync_all_events(cal, self.sync_token)
+
+		print(f"Syncing {self.sync_token} to {str(updated_events.sync_token)}")
+		print("|")
 		for ics in updated_events:
-			if ics.data is None:
-				print(ics.url, "\ndeleted")
-				with db.session() as sess:
-					event = sess.exec(select(Event).where(Event.ics_url == str(ics.url))).all()
-					print(event)
-					#sess.delete(event)
-					sess.commit()
-				print(db.exe(select(Event).where(Event.ics_url == ics.url)).first())
-			else:
-				for comp in icalendar.Event.from_ical(ics.data).walk(name='VEVENT'):
-					print(Event.from_icalendar(comp, str(comp['UID']), ics.url))
-				print(ics.data, "\nchanged")
-				#db.session().exec()
-				#update_event_in_database(event)
-			self.sync_token = str(updated_events.sync_token)
+			if ics.data is not None:
+				# optain last sequence
+				comp = undefined
+				for temp_comp in icalendar.Event.from_ical(ics.data).walk(name='VEVENT'):
+					comp = temp_comp
+
+				#check if event is being deleted
+				if "EXDATE" in comp:
+
+					print(f"| {ics.url} deleted")
+					with db.session() as sess:
+						event = sess.exec(select(Event).where(Event.ics_url == str(ics.url))).all()
+						sess.delete(event)
+						sess.commit()
+				else:
+					#for comp in icalendar.Event.from_ical(ics.data).walk(name='VEVENT'):
+						#print(Event.from_icalendar(comp, str(comp['UID']), ics.url))
+					print(f"| {ics.url} changed")
+					#db.session().exec()
+					with db.session() as sess:
+						event = sess.exec(select(Event).where(Event.ics_url == str(ics.url))).all()
+						event.update_event_in_database(comp)
+						sess.commit()
+
+		print("--------------------")
+
+		#self.sync_token = str(updated_events.sync_token)
 	
 	def _update_events(self, cal_data: list[CalendarObjectResource]|SynchronizableCalendarObjectCollection) -> list[Event]:
 		LOGGER.info("Parsing events...")
