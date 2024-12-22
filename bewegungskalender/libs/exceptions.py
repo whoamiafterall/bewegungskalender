f78b3db88b35e941087cefe98302059df29e9721
Whoami <aberallesigut@riseup.net>
2024-12-22 17:07:06 +0100

    Removed NetworkConnectionError as it is no longer necessary

--- bewegungskalender/libs/exceptions.py
+++ bewegungskalender/libs/exceptions.py
@@ -1,11 +1,5 @@
 # Custom Exception Classes
 
-class NetworkConnectionError(Exception):
-    """A Connection error occurred."""
-    def __init__(self, url):
-        super().__init__(f"\n\nCouldn't get data from {url} due to bad or no network connection.\n\n"
-                                     f"Please check your network connection and rerun the program.")
-
 class NoLinkError(Exception):
     """No Link could be found,"""
     def __init__(self, string:str):
