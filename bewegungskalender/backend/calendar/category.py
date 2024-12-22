15427533bac17f01886d4899d5db7d7119ff5f66
Nina <nina.e@systemli.org>
2024-12-22 14:56:34 +0100

    fixup! Fixed bug with ETA for downloading and creation of database

--- bewegungskalender/backend/calendar/category.py
+++ bewegungskalender/backend/calendar/category.py
@@ -117,9 +117,9 @@ class Category(SQLModel, table=True):
 		for ics in cal_data:
 			counter += 1
 			exact_percent = 100 / ref_status['complete'] * ((ref_status['current']-1)+1 / len(cal_data) * counter)
-			etm = "..." if exact_percent == 0 else round((time.time() - ref_status['start_time']) / exact_percent * 100)
+			eta = "..." if exact_percent == 0 else round((time.time() - ref_status['start_time']) / exact_percent * (100 - exact_percent))
 
-			LOGGER.info(f"Category [{ref_status['current']}/{ref_status['complete']}] Event [{counter}/{len(cal_data)}] - {round(exact_percent,1)}% ETM: {etm}s")
+			LOGGER.info(f"Category [{ref_status['current']}/{ref_status['complete']}] Event [{counter}/{len(cal_data)}] - {round(exact_percent,1)}% ETA: {eta}s")
 
 
 			try:
