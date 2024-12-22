670e8e7c0f95078ab4c97281bf26c7a685b92832
Whoami <aberallesigut@riseup.net>
2024-12-22 17:11:43 +0100

    removed get_sync_token

--- bewegungskalender/backend/calendar/category.py
+++ bewegungskalender/backend/calendar/category.py
@@ -1,4 +1,3 @@
-import math
 import sys
 import time
 
@@ -11,7 +10,7 @@ from sqlmodel import SQLModel, Relationship, Field, select
 from starlette.config import undefined
 
 from bewegungskalender.backend.calendar.client import get_calendar_by_url, get_calendar_name, get_calendar_color, \
-	sync_all_events, get_sync_token, get_all_events, get_upcoming_events
+	sync_all_events, get_all_events, get_upcoming_events
 from bewegungskalender.backend.calendar.event import Event
 from bewegungskalender.backend.formatting.nextcloud_urls import get_ics_url
 from bewegungskalender.backend.io import db
@@ -44,7 +43,6 @@ class Category(SQLModel, table=True):
 			ics_url = get_ics_url(configline['calendar']['public']),
 			emoji = configline['calendar']['emoji'],
 			color = get_calendar_color(cal),
-			sync_token = str(get_sync_token(cal)),
 			map_marker = configline['calendar']['map_marker'],
 			description = configline['calendar']['description'],
 		)
