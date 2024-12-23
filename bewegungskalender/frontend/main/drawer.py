5402f135c28ef86d60c8f76b586dbe9d2f7c84fb
Nina <nina.e@systemli.org>
2024-12-23 15:16:25 +0100

    Reworked location type filtering to use the enum used by events in database

--- bewegungskalender/frontend/main/drawer.py
+++ bewegungskalender/frontend/main/drawer.py
@@ -1,6 +1,7 @@
 from nicegui import ui
 from nicegui.page_layout import RightDrawer, LeftDrawer
 
+from bewegungskalender.backend.calendar.location import EventLocationType
 from bewegungskalender.frontend.filter.filters.category_filter import categories_filters_ui
 from bewegungskalender.frontend.filter.filters.location_proximitry_filter import location_proximity_filter_ui
 from bewegungskalender.frontend.filter.filters.location_type_filter import location_type_filter_ui, LOCATION_TYPE_FILTER
@@ -23,13 +24,14 @@ async def right_drawer() -> RightDrawer:
 			'width=auto') as rd:
 		with ui.column(wrap=False, align_items='start').classes('m-0 gap-1 max-w-1/4 pt-5 px-3 shrink text-sm lg:hidden'):
 
+			duration_filter_ui()
 			location_type_filter_ui().bind_visibility_from(ROUTER,"current_page",lambda e: e is not None and (e.path == "/"))
 
 			# temporary workaround due to nice gui not having the option to bind visibility from multiple attributes
 			with ui.list().classes("w-full").bind_visibility_from(ROUTER, 'current_page',lambda e: e is not None and (e.path == "/karte")):
 				location_proximity_filter_ui()
 			with ui.list().classes("w-full").bind_visibility_from(ROUTER, 'current_page',lambda e: e is not None and (e.path == "/")):
-				location_proximity_filter_ui().bind_visibility_from(LOCATION_TYPE_FILTER, 'state', lambda v: v != "Online")
+				location_proximity_filter_ui().bind_visibility_from(LOCATION_TYPE_FILTER.state,target_name="value",backward=lambda v: (EventLocationType.offline in v))
 
 			await categories_filters_ui()
 	return rd
