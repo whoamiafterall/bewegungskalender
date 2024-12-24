f35398a0eadddc84130004ea6ca74a2ff7fd1e7b
Whoami <aberallesigut@riseup.net>
2024-12-24 20:08:31 +0100

    changed header style to purple to align with MovementWiki

--- bewegungskalender/frontend/main/header.py
+++ bewegungskalender/frontend/main/header.py
@@ -9,12 +9,12 @@ from bewegungskalender.frontend.navigation.router import Router, ROUTER
 
 @contextmanager
 def header(ld:LeftDrawer, rd:RightDrawer=None):
-    with ui.header(elevated=True).classes('fixed h-50px flex-nowrap m-0 px-3 py-2 items-center max-sm:hidden') as head:
+    with ui.header().classes('fixed h-50px flex-nowrap bg-accent m-0 px-3 py-2 items-center max-sm:hidden'):
         with ui.button_group().props('flat'):
             main_menu(ld)
         ui.space().classes('max-sm:hidden')
         with ui.row().classes('max-lg:hidden m-0 p-0'):
-            with ui.button_group().props('outline rounded'):
+            with ui.button_group().props('flat'):
                 secondary_menu(ld)
         ui.space().classes('max-sm:hidden lg:hidden')
 
