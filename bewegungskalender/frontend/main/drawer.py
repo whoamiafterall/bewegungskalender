117858f6040347e9ffe6ba62f915c386ea1e6ec5
whoami <schmusi@systemli.org>
2025-01-15 21:41:14 +0100

    Bugfixes: 1) Added close button, now its closeable on smartphone 2) Fixed Location_Search visibility (because of new LocationTypes from previous commmits)

--- bewegungskalender/frontend/main/drawer.py
+++ bewegungskalender/frontend/main/drawer.py
@@ -6,6 +6,7 @@ from bewegungskalender.frontend.filter.filters.category_filter import categories
 from bewegungskalender.frontend.filter.filters.location_proximitry_filter import location_proximity_filter_ui
 from bewegungskalender.frontend.filter.filters.location_type_filter import location_type_filter_ui, LOCATION_TYPE_FILTER
 from bewegungskalender.frontend.filter.filters.duration_filter import duration_filter_ui
+from bewegungskalender.frontend.filter.filters.time_filter import month_filter_ui
 from bewegungskalender.frontend.main.menu import secondary_menu
 from bewegungskalender.frontend.navigation.router import ROUTER
 
@@ -20,10 +21,10 @@ def left_drawer() -> LeftDrawer:
 	return ld
 
 async def right_drawer() -> RightDrawer:
-	with ui.right_drawer(value=False, fixed=True, top_corner=False).classes('p-0 bg-primary m-0').props(
-			'width=auto') as rd:
-		with ui.column(wrap=False, align_items='start').classes('m-0 gap-1 max-w-1/4 pt-5 px-3 shrink text-sm lg:hidden'):
-
+	with ui.right_drawer(value=False, fixed=True, top_corner=False).classes('p-0 bg-primary lg:hidden m-0').props(
+				'width=auto') as rd:
+		with ui.column(wrap=False, align_items='stretch').classes('m-0 gap-1 pt-5 px-3 shrink text-sm'):
+			
 			duration_filter_ui()
 			location_type_filter_ui().bind_visibility_from(ROUTER,"current_page",lambda e: e is not None and (e.path == "/"))
 
@@ -31,7 +32,9 @@ async def right_drawer() -> RightDrawer:
 			with ui.list().classes("w-full").bind_visibility_from(ROUTER, 'current_page',lambda e: e is not None and (e.path == "/karte")):
 				location_proximity_filter_ui()
 			with ui.list().classes("w-full").bind_visibility_from(ROUTER, 'current_page',lambda e: e is not None and (e.path == "/")):
-				location_proximity_filter_ui().bind_visibility_from(LOCATION_TYPE_FILTER.state, target_name="value", backward=lambda v: (EventLocationType.offline in v))
+				location_proximity_filter_ui().bind_visibility_from(LOCATION_TYPE_FILTER.state, target_name="value", backward=lambda v: (EventLocationType.international or EventLocationType.national or EventLocationType.local in v))
 
 			await categories_filters_ui()
+			ui.button(icon='close', on_click=lambda: rd.hide()).props('flat color=white align=center').classes(
+				'h-24px')
 	return rd
