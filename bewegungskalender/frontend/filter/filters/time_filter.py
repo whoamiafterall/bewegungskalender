e71d909f5baea719593907c4952c1183eb4f896f
Nina <nina.e@systemli.org>
2025-01-16 14:01:35 +0100

    Worked on per month filtering for list view: Added dynamic range for year

--- bewegungskalender/frontend/filter/filters/time_filter.py
+++ bewegungskalender/frontend/filter/filters/time_filter.py
@@ -15,13 +15,13 @@ from bewegungskalender.frontend.filter.filter import call_refresh_filter_event
 def month_filter_ui():
     with ui.button_group().classes('h-10 items-stretch'):
 
-
         months_list = {}
         for i in range(1, 13):
             months_list[i] = calendar.month_name[i]
         years_list = {}
-        for i in range(2018, 2030):
+        for i in range(TIME_FILTER.first_event_year, TIME_FILTER.last_event_year+1):
             years_list[i] = i
+
         ui.button(icon='navigate_before',on_click=TIME_FILTER.decrement_month)
         ui.select(options=months_list,
                 ).props("outline dense behavior=menu color=secondary hide-dropdown-icon menu-offset=[10,10]"
@@ -41,16 +41,18 @@ class TimeFilterController:
     def __init__(self):
         # Get first and last event in database so we know which months exist
         first_event = db.exe(select(Event).order_by(Event.start)).first()
-        last_event =  db.exe(select(Event).order_by(desc(Event.start))).first()
+        last_event = db.exe(select(Event).order_by(desc(Event.start))).first()
+
 
         self.first_event_year = first_event.start.year
-        self.last_event_year =  last_event.end.year
+        self.last_event_year = last_event.end.year
 
         self.month = binding.BindableProperty()
         self.month.value = datetime.now().month
 
         self.year = binding.BindableProperty()
-        self.year.value = datetime.now().year
+
+        self.year.value = max(min(datetime.now().year,self.last_event_year),self.first_event_year)
 
     def decrement_month(self):
         if self.month.value > 1:
