a140e7f9a9a31e52df6bc60df2e94f9ce8833a94
whoami <schmusi@systemli.org>
2025-01-29 09:55:44 +0100

    Improved: LocationTypes now consist of a keyword and an icon :) Renamed: EventLocationType to LocationType

--- bewegungskalender/frontend/filter/filters/location_type_filter.py
+++ bewegungskalender/frontend/filter/filters/location_type_filter.py
@@ -2,24 +2,14 @@ from nicegui import ui,binding
 from nicegui.element import Element
 from sqlalchemy import Select
 
-from bewegungskalender.backend.calendar.location import Location, EventLocationType
+from bewegungskalender.backend.calendar.location import Location, LocationType
 from bewegungskalender.frontend.filter.filter import call_refresh_filter_event
 
 def location_type_filter_ui() -> Element:
 
-	return (ui.select(
-        {EventLocationType.online: 'Online'
-            , EventLocationType.local: 'Lokal'
-            , EventLocationType.national: 'Überregional'
-            , EventLocationType.international: 'International'
-            , EventLocationType.undefined: 'Unbekannt'}
-        , label="Ort", value=[
-            EventLocationType.online,
-            EventLocationType.local,
-            EventLocationType.national,
-            EventLocationType.international,
-            EventLocationType.undefined
-        ],multiple=True, clearable=True)
+    return (ui.select(
+        {location_type: location_type.keyword for location_type in LocationType}
+        , label="Ort", value=[location_type for location_type in LocationType], multiple=True, clearable=True)
     .on_value_change(call_refresh_filter_event).bind_value(LOCATION_TYPE_FILTER.state)
     .classes("pl-3 w-full my-1")).props("outline behavior=menu dense hide-dropdown-icon color=secondary")
 
@@ -31,7 +21,7 @@ class LocationTypeFilterController:
     def apply_filter_to_statement(self,statement: Select):
 
         if len(self.state.value) > 0:
-            for event_type in [e.value for e in EventLocationType]:
+            for event_type in LocationType:
                 if event_type not in self.state.value:
                     statement = statement.where(Location.type != event_type)
 
@@ -41,10 +31,10 @@ class LocationTypeFilterController:
 LOCATION_TYPE_FILTER = LocationTypeFilterController()
 
 
-class OfflineLocationTypeFilterController:
+class LocalLocationTypeFilterController:
 
     def apply_filter_to_statement(self,statement: Select):
-        return statement.where(Location.type == EventLocationType.local)
+        return statement.where(Location.type == LocationType.local)
 
 
-OFFLINE_LOCATION_TYPE_FILTER = OfflineLocationTypeFilterController()
+LOCAL_LOCATION_TYPE_FILTER = LocalLocationTypeFilterController()
