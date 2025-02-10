f14cb3274a830ce8aaa27ebcca7f4a08c17cfd70
whoami <schmusi@systemli.org>
2025-02-10 23:20:29 +0100

    Added: month_filter_ui() to header. Removed: Filter button on md screens - now using page sticky (floating button).

--- bewegungskalender/frontend/layout/header.py
+++ bewegungskalender/frontend/layout/header.py
@@ -3,20 +3,30 @@ from contextlib import contextmanager
 from nicegui import ui
 from nicegui.page_layout import LeftDrawer, RightDrawer
 
+from bewegungskalender.frontend.filter.filters.time_filter import month_filter_ui
 from bewegungskalender.frontend.layout.menu import main_menu, secondary_menu
-from bewegungskalender.frontend.navigation.router import Router, ROUTER
+from bewegungskalender.frontend.navigation.router import ROUTER
 
 
 @contextmanager
-def header(ld:LeftDrawer, rd:RightDrawer=None):
-    with ui.header().classes('fixed h-50px flex-nowrap bg-accent m-0 px-3 py-2 items-center max-sm:hidden'):
-        with ui.button_group().props('flat'):
-            main_menu(ld)
-        ui.space().classes('max-sm:hidden')
-        with ui.row().classes('max-lg:hidden m-0 p-0'):
-            with ui.button_group().props('flat'):
-                secondary_menu(ld)
-        ui.space().classes('max-sm:hidden lg:hidden')
+def header(ld: LeftDrawer, rd: RightDrawer):
+	with ui.header().classes('fixed h-50px max-sm:hidden flex-nowrap bg-accent m-0 px-3 py-2 items-center'):
+		with ui.button_group().props('flat dense'):
+			main_menu(ld, rd, props='flat')
+		ui.space().classes()
+		with filter_visibility(''):
+			month_filter_ui()
+		ui.space().classes('max-lg:hidden')
+		with ui.row().classes('max-lg:hidden'):
+			with ui.button_group().props('flat'):
+				secondary_menu(ld)
 
-        ui.button("Filter", icon='filter_alt', on_click=lambda: rd.toggle()).props(
-            "flat color=white").classes('max-sm:hidden lg:hidden').bind_visibility_from(ROUTER, "current_page", backward=lambda e: e is not None and (e.path == "/" or e.path == "/karte"))
+
+@contextmanager
+def filter_visibility(classes: str = ''):
+	with ui.element().bind_visibility_from(ROUTER, "current_page",
+	                                       backward=lambda
+			                                       e: e is not None and (
+			                                       e.path == "/")) as wrapper:
+		wrapper.classes(classes)
+		yield wrapper
