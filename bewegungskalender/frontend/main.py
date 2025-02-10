e2847b2de6b7e1792596db865a1b8207714b3176
whoami <schmusi@systemli.org>
2025-02-10 23:53:08 +0100

    Added: Right Drawer to Main Menu and header/footer Moved: Manifest and ServiceWorker to helpers

--- bewegungskalender/frontend/main.py
+++ bewegungskalender/frontend/main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from nicegui import ui
 
 from bewegungskalender.backend.io.config import UI_TITLE, UI_FAVICON, UI_PORT
@@ -14,31 +16,34 @@ from bewegungskalender.libs.logger import LOGGER
 @ui.page('/')
 @ui.page('/{_:path}')
 async def main_page():
-    await ui.context.client.connected()
+	await ui.context.client.connected()
+	
+	# Load the theme from browser storage
+	Theme.init_theme()
+	
+	# Modify default page container
+	ui.query('.nicegui-content').classes('p-0 min-h-full overflow-hidden')
+	# Create an Invisible frame that shows different views and fits underneath the header / above the footer
+	ROUTER.frame().classes('w-screen h-[calc(100vh-55px)]')
+	
+	# Create visible Layout
+	ld = left_drawer()
+	rd = await right_drawer()
+	header(ld, rd)
+	filter_sticky(rd)
+	footer(ld, rd)
 
-    # Load the theme from browser storage
-    Theme.init_theme()
-    
-    # Modify default page container
-    ui.query('.nicegui-content').classes('p-0 min-h-full overflow-auto')
-    # Create an Invisible frame that shows different views and fits underneath the header / above the footer
-    ROUTER.frame().classes('w-screen h-[calc(100vh-55px)]')
-    
-    # Create visible Layout
-    ld = left_drawer()
-    rd = await right_drawer()
-    header(ld, rd)
-    filter_sticky(rd)
-    footer(ld)
 
 def start_ui():
-    LOGGER.debug('Starting UI...')
-    ui.add_head_html('<link rel="manifest" href="/manifest.json">')
-    ui.add_head_html(
-        '<script>if("serviceWorker" in navigator) { navigator.serviceWorker.register("/service_worker.js"); };</script>')
-    ui.run(title=UI_TITLE, favicon=UI_FAVICON, language='de', port=UI_PORT, uvicorn_logging_level='info',storage_secret=UI_STORAGE_SECRET)
-    LOGGER.debug('Successfully started UI.')
+	LOGGER.debug('Starting UI...')
+	ui.add_head_html('<link rel="manifest" href="/helpers/manifest.json">')
+	ui.add_head_html(
+		'<script>if("serviceWorker" in navigator) { navigator.serviceWorker.register("/helpers/service_worker.js"); };</script>')
+	ui.run(title=UI_TITLE, favicon=UI_FAVICON, language='de', port=UI_PORT, uvicorn_logging_level='info',
+	       storage_secret=UI_STORAGE_SECRET)
+	LOGGER.debug('Successfully started UI.')
+
 
 # Run as Module or Standalone program
 if __name__ in {"__main__", "__mp_main__"}:
-    start_ui()
+	start_ui()
