d4590a5a47e6535e1a94805cd34f511ac77fade2
whoami <schmusi@systemli.org>
2025-02-11 00:04:38 +0100

    Moved: months_list creation to controller

--- bewegungskalender/frontend/filter/filters/time_filter.py
+++ bewegungskalender/frontend/filter/filters/time_filter.py
@@ -10,65 +10,72 @@ from bewegungskalender.frontend.filter.filter import call_refresh_filter_event
 
 
 def month_filter_ui():
-    with ui.button_group().classes('h-10 items-stretch').props('dense flat'):
+	with ((ui.button_group().classes('h-10 items-stretch').props('dense flat'))):
+		ui.button(icon='navigate_before', on_click=MONTH_FILTER.decrement_month
+		          ).props('dense text-color=secondary')
+		ui.select(options=MONTH_FILTER.months_list,
+		          ).props("dense filled behavior=menu hide-dropdown-icon color=secondary menu-offset=[10,10]"
+		                  ).classes('font-medium bg-primary'
+		                            ).bind_value(MONTH_FILTER.month,
+		                                         ).on_value_change(call_refresh_filter_event)
+		ui.button(icon='navigate_next', on_click=MONTH_FILTER.increment_month
+		          ).props('dense text-color=secondary')
+		ui.select(options=MONTH_FILTER.years_list,
+		          ).props("dense filled behavior=menu hide-dropdown-icon color=secondary menu-offset=[10,10]"
+		                  ).classes('font-medium pr-3 rounded-r-sm bg-primary'
+		                            ).bind_value(MONTH_FILTER.year,
+		                                         ).on_value_change(call_refresh_filter_event)
 
-        months_list = {}
-        for i in range(1, 13):
-            months_list[i] = calendar.month_name[i]
-        years_list = {}
-        for i in range(datetime.now().year-1, datetime.now().year+1):
-            years_list[i] = i
-
-        ui.button(icon='navigate_before', on_click=MONTH_FILTER.decrement_month).props('dense text-color=secondary')
-        ui.select(options=months_list,
-                ).props("dense outlined behavior=menu hide-dropdown-icon color=secondary menu-offset=[10,10]"
-                ).classes('font-medium'
-                ).bind_value(MONTH_FILTER.month,
-                             ).on_value_change(call_refresh_filter_event)
-        ui.button(icon='navigate_next', on_click=MONTH_FILTER.increment_month).props('dense text-color=secondary')
-        ui.select(options=years_list,
-                ).props("dense outlined behavior=menu hide-dropdown-icon color=secondary menu-offset=[10,10]"
-                ).classes('font-medium pr-3 rounded-r-sm'
-                ).bind_value(MONTH_FILTER.year,
-                             ).on_value_change(call_refresh_filter_event)
 
 class MonthFilterController:
-    def __init__(self):
-        # Get first and last event in database so we know which months exist
-        self.month = binding.BindableProperty()
-        self.month.value = datetime.now().month
-        self.year = binding.BindableProperty()
-        self.year.value = datetime.now().year
+	
+	def __init__(self):
+		# Get first and last event in database so we know which months exist
+		self.month = binding.BindableProperty()
+		self.month.value = datetime.now().month
+		self.year = binding.BindableProperty()
+		self.year.value = datetime.now().year
+		self.months_list = {}
+		for i in range(1, 13):
+			self.months_list[i] = calendar.month_name[i]
+		self.years_list = {}
+		for i in range(datetime.now().year - 1, datetime.now().year + 1):
+			self.years_list[i] = i
+	
+	def decrement_month(self):
+		if self.month.value > 1:
+			self.month.value = self.month.value - 1
+		else:
+			self.month.value = 12
+			self.year.value = self.year.value - 1
+	
+	def increment_month(self):
+		if self.month.value < 12:
+			self.month.value = self.month.value + 1
+		else:
+			self.month.value = 1
+			self.year.value = self.year.value + 1
+	
+	# TODO implement a proper where statement
+	
+	def apply_filter_to_statement(self, statement: Select):
+		month_begin = datetime(self.year.value, self.month.value, 1)
+		month_end = datetime(self.year.value, self.month.value + 1, 1) if self.month.value + 1 <= 12 else datetime(
+			self.year.value + 1, 1, 1)
+		
+		return statement.where(
+			or_(and_(Event.start > month_begin, Event.start < month_end)
+			    , and_(Event.end > month_begin, Event.end < month_end))
+		)
 
-    def decrement_month(self):
-        if self.month.value > 1:
-            self.month.value = self.month.value - 1
-        else:
-            self.month.value = 12
-            self.year.value = self.year.value - 1
 
-    def increment_month(self):
-        if self.month.value < 12:
-            self.month.value = self.month.value + 1
-        else:
-            self.month.value = 1
-            self.year.value = self.year.value + 1
-        # TODO implement a proper where statement
-    def apply_filter_to_statement(self,statement: Select):
-        month_begin = datetime(self.year.value, self.month.value,1)
-        month_end = datetime(self.year.value, self.month.value+1,1) if self.month.value+1 <= 12 else datetime(self.year.value+1, 1,1)
-
-        return statement.where(
-            or_(and_(Event.start > month_begin,Event.start < month_end)
-                ,and_(Event.end > month_begin,Event.end < month_end))
-        )
-   
 MONTH_FILTER = MonthFilterController()
 
 
 class FromTodayFilterController:
+	
+	def apply_filter_to_statement(self, statement: Select):
+		return statement.where(Event.start > datetime.now())
 
-    def apply_filter_to_statement(self,statement: Select):
-        return statement.where(Event.start > datetime.now())
 
 FROM_TODAY_FILTER = FromTodayFilterController()
