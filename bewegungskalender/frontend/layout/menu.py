0b900d941d587d1789e2401b706fb49d924bb969
whoami <schmusi@systemli.org>
2025-02-10 23:22:46 +0100

    Added: Left_Drawer_Button Added: Filter_Button on small screens (Footer) Removed: Unused Menu_Items

--- bewegungskalender/frontend/layout/menu.py
+++ bewegungskalender/frontend/layout/menu.py
@@ -4,7 +4,6 @@ from nicegui import ui, app
 from nicegui.elements.button import Button
 from nicegui.page_layout import LeftDrawer, RightDrawer
 
-from bewegungskalender.backend.formatting.nextcloud_urls import NC_MONTH_VIEW, NC_YEAR_VIEW
 from bewegungskalender.backend.io.config import MENU
 from bewegungskalender.frontend.layout.theme import Theme
 from bewegungskalender.frontend.navigation.router import ROUTER
@@ -16,36 +15,37 @@ from bewegungskalender.frontend.views.map import map_view
 
 
 class MenuButton(Button):
-    def __init__(self, item:dict, view:Callable, drawer:LeftDrawer|RightDrawer = None) -> None:
-        self.item = item
-        self.view = view
-        self.drawer = drawer
-        super().__init__(item['label'], icon=item['icon'],
-            on_click=lambda: self._open_view_close_drawer())
-        
-    def _open_view_close_drawer(self) -> Any:
-        ROUTER.open(self.view)
-        if self.drawer:
-            self.drawer.hide()
+	def __init__(self, item: dict, view: Callable, drawer: LeftDrawer | RightDrawer = None) -> None:
+		self.item = item
+		self.view = view
+		self.drawer = drawer
+		super().__init__(item['label'], icon=item['icon'],
+		                 on_click=lambda: self._open_view_close_drawer())
+	
+	def _open_view_close_drawer(self) -> Any:
+		ROUTER.open(self.view)
+		if self.drawer:
+			self.drawer.hide()
 
 
 class NavigateButton(Button):
-    def __init__(self, icon: str, text: str, link: str) -> None:
-        super().__init__(text=text, icon=icon, on_click=lambda: ui.navigate.to(link, new_tab=True))
+	def __init__(self, icon: str, text: str, link: str) -> None:
+		super().__init__(text=text, icon=icon, on_click=lambda: ui.navigate.to(link, new_tab=True))
 
 
-def main_menu(left_drawer:LeftDrawer, classes:str=None, props:str='color=accent text-color=white'):
-    MenuButton(MENU['list'], list_view).classes(classes).props(props)
-    MenuButton(MENU['map'], map_view).classes(classes).props(props)
-    MenuButton(MENU['form'], form_view, left_drawer).classes(classes).props(props)
-   # NavigateButton('calendar_month', 'Monat', NC_MONTH_VIEW).classes(classes).props(props)
-    NavigateButton('grid_on', 'Jahr', NC_YEAR_VIEW).classes(classes).props(props)
-  #  MenuButton(MENU['calendar'], calendar_view).classes(classes).props(props)
-#    MenuButton(MENU['calendar'], custom_calendar_view).props(props)
-  #  MenuButton(MENU['table'], table_view).props(props)
+def main_menu(left_drawer: LeftDrawer, right_drawer: RightDrawer, classes: str = None,
+              props: str = 'flat dense color=accent text-color=white'):
+	ui.button(on_click=lambda: left_drawer.toggle(), icon='menu').classes(classes).props(props).tailwind('lg:hidden')
+	MenuButton(MENU['list'], list_view).classes(classes).props(props)
+	MenuButton(MENU['map'], map_view).classes(classes).props(props)
+	MenuButton(MENU['form'], form_view, left_drawer).classes(classes).props(props)
+	ui.button(icon='filter_alt', on_click=lambda: right_drawer.toggle()).props(props).classes(classes).tailwind(
+		'sm:hidden')
 
-def secondary_menu(left_drawer:LeftDrawer, classes:str=None, props:str='color=accent text-color=white'):
-    MenuButton(MENU['about'], about_view, left_drawer).classes(classes).props(props)
-    #MenuButton(MENU['FAQ'], faq_view, left_drawer).classes(classes).props(props)
-    MenuButton(MENU['links'], links_view, left_drawer).classes(classes).props(props)
-    ui.button(on_click=lambda:Theme.toggle_dark()).classes(classes).props(props).bind_icon_from(app.storage.user,"dark_mode", lambda v: 'dark_mode' if v == False else 'light_mode')#.bind_text_from(Theme,"dark_mode", lambda v: 'Darkmode' if v == False else 'Lightmode')
+
+def secondary_menu(left_drawer: LeftDrawer, classes: str = None, props: str = 'color=accent text-color=white'):
+	MenuButton(MENU['about'], about_view, left_drawer).classes(classes).props(props)
+	MenuButton(MENU['links'], links_view, left_drawer).classes(classes).props(props)
+	ui.button(on_click=lambda: Theme.toggle_dark()).classes(classes).props(props).bind_icon_from(app.storage.user,
+	                                                                                             "dark_mode", lambda
+		                                                                                             v: 'dark_mode' if v == False else 'light_mode')  # .bind_text_from(Theme,"dark_mode", lambda v: 'Darkmode' if v == False else 'Lightmode')
