6ca59e964cf7c5358662f2f6b4631846f73e5a8e
whoami <schmusi@systemli.org>
2025-02-11 00:06:54 +0100

    Removed: location_type_filter_ui

--- bewegungskalender/frontend/layout/drawer.py
+++ bewegungskalender/frontend/layout/drawer.py
@@ -25,13 +25,12 @@ async def right_drawer() -> RightDrawer:
 		with ui.column(wrap=False, align_items='stretch').classes('m-0 gap-1 pt-5 px-3 shrink text-sm'):
 			
 			duration_filter_ui()
-			location_type_filter_ui().bind_visibility_from(ROUTER,"current_page",lambda e: e is not None and (e.path == "/"))
 
 			# temporary workaround due to nice gui not having the option to bind visibility from multiple attributes
 			with ui.list().classes("w-full").bind_visibility_from(ROUTER, 'current_page',lambda e: e is not None and (e.path == "/karte")):
 				location_proximity_filter_ui()
 			with ui.list().classes("w-full").bind_visibility_from(ROUTER, 'current_page',lambda e: e is not None and (e.path == "/")):
-				location_proximity_filter_ui().bind_visibility_from(LOCATION_TYPE_FILTER.state, target_name="value", backward=lambda v: (LocationType.international or LocationType.national or LocationType.local in v))
+				location_proximity_filter_ui()#.bind_visibility_from(LOCATION_TYPE_FILTER.state, target_name="value", backward=lambda v: (LocationType.international or LocationType.national or LocationType.local in v))
 
 			await categories_filters_ui()
 			ui.button(icon='close', on_click=lambda: rd.hide()).props('flat color=white align=center').classes(
