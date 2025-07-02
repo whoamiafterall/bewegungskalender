58e8dd83f6dd4bbda2dc163f0fa1f209e6b42ce5
Nina <nina.e@systemli.org>
2025-07-02 09:17:53 +0200

    Add event id as foreign_key to event instance

--- bewegungskalender/backend/calendar/event_instance.py
+++ bewegungskalender/backend/calendar/event_instance.py
@@ -1,13 +1,7 @@
 from datetime import datetime, timedelta, time, date
 from typing import TYPE_CHECKING
 
-import icalendar
-from icalendar.cal import Component
 from sqlmodel import SQLModel, Field, Relationship
-
-from bewegungskalender.backend.calendar.location import Location, parse_location
-from bewegungskalender.backend.formatting.format import get_link
-from bewegungskalender.backend.io.config import TIMEZONE
 
 if TYPE_CHECKING:  # Necessary for SQLModel Relationships across Files
 	from bewegungskalender.backend.calendar.category import Category
@@ -17,6 +11,8 @@ class EventInstance(SQLModel, table=True):
 	id: int = Field(default=None, primary_key=True)
 	event: "Event" = Relationship(back_populates="event_instances",
 								  sa_relationship_kwargs={"lazy": "selectin"})
+	event_id: int = Field(foreign_key="event.id", ondelete="CASCADE")
+
 	start: datetime = Field(index=True)
 	end: datetime
 	duration: timedelta = Field(index=True)
